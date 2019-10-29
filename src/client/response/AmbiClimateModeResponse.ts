336f9f1ab6d3c36ee7b9325223a89d6d8c8b934f
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2019-10-29 18:02:20 +0700

    Change mode into enum

--- src/client/response/AmbiClimateModeResponse.ts
+++ src/client/response/AmbiClimateModeResponse.ts
@@ -1,4 +1,14 @@
+enum Mode {
+    AWAY_HUMIDITY_UPPER='Away_Humidity_Upper',
+    AWAY_TEMPERATURE_LOWER='Away_Temperature_Lower',
+    AWAY_TEMPERATURE_UPPER='Away_Temperature_Upper',
+    COMFORT='Comfort',
+    MANUAL='Manual',
+    OFF='Off',
+    TEMPERATURE='Temperature',
+}
+
 export interface AmbiClimateModeResponse {
-  mode: String;
+  mode: Mode;
   value: Number | null;
 }
